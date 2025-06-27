c6ce0b154e61daf2b06f2f80d4d6f450c5c356c4
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-26 23:56:02 -0700

    leetcode two sums

---
+++ twoSums.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class twoSums {
+    public static void main(String[] args) {
+
+    }
+    public int[] twoSum(int[] nums, int target){
+        for(int i=0;i< nums.length;i++){
+            if(target-nums[i]){
+
+            }
+        }
+
+    }
+}
