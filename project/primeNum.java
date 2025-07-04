ca75dd263b2d45b5da1f382164389a3cb027671b
lazkopat3522 <ikramamzkg@gmail.com>
2025-07-03 23:57:55 -0700

    prime

---
+++ project/primeNum.java
@@ -0,0 +1,23 @@
+package project;
+
+import java.util.Scanner;
+
+public class primeNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(calcPrime(n));
+    }
+    public static String calcPrime(int n){
+        int c=0;
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0){
+                c++;
+            }
+        }
+        if(c==1){
+            return "YES";
+        } else
+            return "NO";
+    }
+}
