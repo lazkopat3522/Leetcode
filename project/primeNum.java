86e6bb86cfe3e1da13757e5ccff6bcf693bb7f1f
lazkopat3522 <ikramamzkg@gmail.com>
2025-07-04 00:07:22 -0700

    all primes

--- project/primeNum.java
+++ project/primeNum.java
@@ -6,18 +6,20 @@ public class primeNum {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.println(calcPrime(n));
+        calcPrime(n);
     }
-    public static String calcPrime(int n){
+    public static void calcPrime(int n){
         int c=0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                c++;
-            }
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    c++;
+                }
+                if (c == 1) {
+                    System.out.print(i+" ");
+                }
+                c = 0;
+
         }
-        if(c==2){
-            return "YES";
-        } else
-            return "NO";
     }
 }
