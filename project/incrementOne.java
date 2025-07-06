a3640eec76922c281d841b8572b23ed9fd2ac7c0
lazkopat3522 <ikramamzkg@gmail.com>
2025-07-05 23:46:13 -0700

    incrementing last digit by one

---
+++ project/incrementOne.java
@@ -0,0 +1,15 @@
+package project;
+
+public class incrementOne {
+    public static void main(String[] args) {
+
+    }
+    public int[] plusOne(int[] digits) {
+        int lastIndex= digits.length;
+        if(digits[lastIndex-1]+1>9){
+            digits[lastIndex-2]=digits[lastIndex-2]+1;
+        } else
+        return digits[]
+    }
+}
+
