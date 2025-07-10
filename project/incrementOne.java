253be3e3909728d2bf2b90746e2ee0a4c4e6020a
lazkopat3522 <ikramamzkg@gmail.com>
2025-07-09 23:55:29 -0700

    incrementing last digit by one

--- project/incrementOne.java
+++ project/incrementOne.java
@@ -2,7 +2,7 @@ package project;
 
 public class incrementOne {
     public static void main(String[] args) {
-        int[] ar={1,0,0,9};
+        int[] ar={1,9,9,9};
         int[] newChico=plusOne(ar);
         for(int i=0; i<ar.length;i++){
             System.out.print(newChico[i]+" ");
@@ -20,7 +20,7 @@ public class incrementOne {
             while(lastNum+1>9) {
                 newArr[lastIndex-c]=(lastNum+1)%10;
                 c++;
-                lastNum=newArr[lastIndex-c]+1;
+                lastNum=newArr[lastIndex-c];
             }
             newArr[lastIndex-c]=newArr[lastIndex-c]+1;
         } else newArr[lastIndex-1]=lastNum+1;
