aee41891f8de79e9de9b4839c64dad0c34273ea9
lazkopat3522 <ikramamzkg@gmail.com>
2025-07-11 23:58:53 -0700

    increment improvisation

---
+++ project/incrementIkram.java
@@ -0,0 +1,11 @@
+package project;
+
+public class incrementIkram {
+    public static void main(String[] args) {
+        int[] ar={9};
+        int num=0;
+        for(int i=0;i< ar.length;i++){
+        num=num+ar[i+1]
+        }
+    }
+}
