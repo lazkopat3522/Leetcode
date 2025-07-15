0ddc29df1ab781f4010200f4b2b29356f67dfb64
lazkopat3522 <ikramamzkg@gmail.com>
2025-07-14 23:39:17 -0700

    hacker rank

--- project/hackerRank.java
+++ project/hackerRank.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class hackerRank {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String st=sc.nextLine();
         System.out.println("================================");
-        for(int i=0;i<3;i++){
-            String s1=sc.next();
-            int x=sc.nextInt();
-            //Complete this line
+        for(int i=0;i<15;i++){
+            if(st[i]==" "){
+                st[12]=st[i+1];
+                st[13]=st[i+2];
+                st[14]=st[i+3];
+            }
+            System.out.print(st[i]);
         }
         System.out.println("================================");
 
